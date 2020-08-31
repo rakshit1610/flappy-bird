dbe54ef507459f275dc7b9ffb9aa99cad2b66e38
Manas-saxena <manassaxena160601@gmail.com>
2020-08-31 18:59:16 +0530

    glitches solved and made code better

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,15 @@
-function mymove() {
+function start() {
     var elem = document.getElementById("bird");   
     var top =200;
-    
-   setInterval(frame, 5);
-    function frame() {
-      if(top<550){
+   setInterval(gravity, 5);
+    function gravity() {
+      if(top<550){                        //adding gravity to bird
               top++; 
         elem.style.top = top + "px"; 
-        console.log(top);
       }
     }
     document.addEventListener("click",function (e){
-        if(top>30){
+        if(top>30){                                                 //makes  bird jump
         top-=100;
         elem.style.top = top + "px";
         } 
