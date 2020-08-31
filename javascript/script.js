044c597b512ed555b0fa2960738f8953879f28fc
Manas-saxena <manassaxena160601@gmail.com>
2020-08-31 23:16:23 +0530

    git commit -m

--- javascript/script.js
+++ javascript/script.js
@@ -1,51 +1,50 @@
-function mymove() {
-    var elem = document.getElementById("bird");   
-    var top =50;
-    
-   setInterval(frame, 5);
-    function frame() {
-      if(top<465){
-              top++; 
-        elem.style.top = top + "px"; 
-        console.log(top);
-      }
+function start() {
+  var elem = document.getElementById("bird");   
+  var top =50;
+  
+ setInterval(frame, 7);
+  function frame() {
+    if(top<465){
+            top++; 
+      elem.style.top = top + "px"; 
+      console.log(top);
     }
-    document.addEventListener("click",function (){
-        if(top>30){
-        top-=100;
-        elem.style.top = top + "px";
-        } 
-    });
+  }
+  document.addEventListener("click",function (){
+      if(top>30){
+      top-=100;
+      elem.style.top = top + "px";
+      } 
+  });
 
-    function pipeCreator(){
-      var pipeLeft= 700;
-      var Heightvar= Math.random()*70;
-      var pipeBottom= Heightvar;
-      var pipe= document.createElement('div');
-      pipe.classList.add('pipe');
-      document.querySelector('.frame').appendChild(pipe);
-      pipe.style.bottom= pipeBottom + "px";
+  function pipeCreator(){
+    var pipeLeft= 700;
+    var Heightvar= Math.random()*70;
+    var pipeBottom= Heightvar;
+    var pipe= document.createElement('div');
+    pipe.classList.add('pipe');
+    document.querySelector('.frame').appendChild(pipe);
+    pipe.style.bottom= pipeBottom + "px";
+    pipe.style.left= pipeLeft + "px";
+
+
+    var id= setInterval(movepipe, 10);
+
+    function movepipe(){
+      pipeLeft--;
       pipe.style.left= pipeLeft + "px";
-  
-  
-      var id= setInterval(movepipe, 10);
-  
-      function movepipe(){
-        pipeLeft--;
-        pipe.style.left= pipeLeft + "px";
-  
-        if (pipeLeft===-80){
-          clearInterval(id);
-          document.querySelector('.frame').removeChild(pipe);
-        }
+
+      if (pipeLeft===-80){
+        clearInterval(id);
+        document.querySelector('.frame').removeChild(pipe);
       }
-  
-      setTimeout(pipeCreator,3500);
     }
-  
-    pipeCreator();
-  
-  
+
+    setTimeout(pipeCreator,3500);
   }
 
-  
+  pipeCreator();
+
+
+}
+
