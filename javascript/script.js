e1bd8726fa34d13eb3db6452be44ae25f1ff1efb
rakshit1610 <rakshitpanjwani@gmail.com>
2020-08-31 17:38:33 +0530

    added pipes of random height created every 3.5 secs

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,51 @@
 function mymove() {
     var elem = document.getElementById("bird");   
-    var top =200;
+    var top =50;
     
    setInterval(frame, 5);
     function frame() {
-      if(top<550){
+      if(top<465){
               top++; 
         elem.style.top = top + "px"; 
         console.log(top);
       }
     }
-    document.addEventListener("click",function (e){
+    document.addEventListener("click",function (){
         if(top>30){
         top-=100;
         elem.style.top = top + "px";
         } 
     });
+
+    function pipeCreator(){
+      var pipeLeft= 700;
+      var Heightvar= Math.random()*70;
+      var pipeBottom= Heightvar;
+      var pipe= document.createElement('div');
+      pipe.classList.add('pipe');
+      document.querySelector('.frame').appendChild(pipe);
+      pipe.style.bottom= pipeBottom + "px";
+      pipe.style.left= pipeLeft + "px";
+  
+  
+      var id= setInterval(movepipe, 10);
+  
+      function movepipe(){
+        pipeLeft--;
+        pipe.style.left= pipeLeft + "px";
+  
+        if (pipeLeft===-80){
+          clearInterval(id);
+          document.querySelector('.frame').removeChild(pipe);
+        }
+      }
+  
+      setTimeout(pipeCreator,3500);
+    }
+  
+    pipeCreator();
+  
   
   }
+
+  
